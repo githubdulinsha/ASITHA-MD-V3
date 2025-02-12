c365ba5777e59adbe60c0f65b36ce1bcbe8734fc
githubdulinsha <dulinshadulinsha224@gmail.com>
2025-02-12 08:19:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=y3BEDTxB#7TM0bd2LZapHwFeuwwTwz53ozsOcPjLRqtJgYwvn1NU' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=S7xzHQzR#2Fjn5IuL3S87fIQHJe1kCQdA05NgvIgT79kjPX4tudg' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94787072548,94704227534' : process.env.SUDO,
